eaf43505aa28f9a426cece68628c0573b8d29eba
Dario Caruso <darioyzf@gmail.com>
2016-11-30 20:58:59 +0100

    mobile tests

---
+++ run/mobile.ts
@@ -0,0 +1,15 @@
+import linet from '../index';
+const verb=require('verbo');
+
+const options=require('./mobileconf.json')
+
+options.wpasupplicant_path=__dirname+'/wpa_supplicant.conf'
+
+let LINET=new linet(options);
+console.log(LINET)
+ LINET.connection().then(function(status){
+  verb(status,'info','LINETWORKING init')
+ }).catch(function(err){
+   console.log('error')
+  verb(err,'error','LINETWORKING init')
+ })
